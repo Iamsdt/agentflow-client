d00be6b622f8e2781b9fe454715a8edde6d1da92
Shudipto Trafder <shudiptotrafder@gmail.com>
2025-10-19 01:06:32 +0600

    refactor: Remove AgentFlowClient and related ping functionality

--- dist/client/index.js
+++
@@ -1,4 +0,0 @@
-// Re-export client functionality
-export * from './client.js';
-// Export ping Response types
-export * from './endpoints/ping.js';
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-// Re-export client-related types and classes
-export * from './client/index.js';
-// Re-export tool-related types and classes
-export * from './tools.js';
